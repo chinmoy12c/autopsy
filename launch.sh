ba1f83ed7bfd46a2cff500174b5692f3337930d1
Kevin Zhu <kevizhu@cisco.com>
2016-07-27 17:07:00 -0700

    relative paths in launch.sh

--- launch.sh
+++ launch.sh
@@ -4,7 +4,7 @@ mkdir -p database
 mkdir -p uploads
 alias kf="pkill -9 python; pkill -9 flask"
 alias fr="flask run --with-threads"
-source "/home/kevizhu/Autopsy/venv/bin/activate"
-source "/home/kevizhu/Autopsy/.p4config"
+source $PWD/venv/bin/activate"
+source $PWD/.p4config"
 export "FLASK_APP=autopsy.py"
 export "FLASK_DEBUG=1"
