4c3cdd0b7c68c87dbab214da0e27b930aa626b6f
Kevin Zhu <kevizhu@cisco.com>
2017-06-13 23:15:52 +0000

    easy install

---
+++ install.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+cd ..
+git clone https://wwwin-gitlab-sjc.cisco.com/SSLMIDPATH/clientlessGDB.git
+echo "Enter your Perforce ticket: "
+read ticket
+echo ${ticket} > clientlessGDB/ticket.txt
+mkdir -p python
+cd python
+python_dir=$(pwd)
+wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tar.xz
+tar -xf Python-3.6.1.tar.xz
+rm Python-3.6.1.tar.xz
+cd Python-3.6.1
+./configure --prefix=${python_dir}
+make
+make test
+make install
+cd ../..
+wget https://github.com/pypa/virtualenv/archive/15.1.0.tar.gz
+tar -xf 15.1.0.tar.gz
+rm 15.1.0.tar.gz
+python virtualenv-15.1.0/virtualenv.py -p python/bin/python3.6 Autopsy/venv
+cd Autopsy
+. launch.sh
+pip install Flask requests requests-ntlm pexpect gunicorn
+flask initdb
+chmod 777 database/cores.db
+deactivate
+cd ..
+mkdir -p nginx
+cd nginx
+nginx_dir = $(pwd)
+wget http://nginx.org/download/nginx-1.13.1.tar.gz
+tar -xf nginx-1.13.1.tar.gz
+rm nginx-1.13.1.tar.gz
+cd nginx-1.13.1
+./configure --with-http_ssl_module --prefix=${nginx_dir}
+make
+make install
+cat << 'EOF' > conf/nginx.conf
+worker_processes    1;
+
+events {
+    worker_connections  1024;
+}
+
+http {
+    include             mime.types;
+    default_type        application/octet-stream;
+
+    access_log          logs/access.log;
+    error_log           logs/error.log;
+
+    sendfile            on;
+    keepalive_timeout   9999;
+
+    upstream app_servers {
+        server  127.0.0.1:5432;
+    }
+
+    server {
+        listen  80;
+        return  301 https://$host$request_uri;
+    }
+
+    server {
+        listen                  443 ssl;
+        client_max_body_size    0;
+
+        location / {
+            proxy_pass          http://app_servers;
+            proxy_redirect      off;
+            proxy_set_header    Host $host;
+            proxy_set_header    X-Real-IP $remote_addr;
+            proxy_set_header    X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header    X-Forwarded-Proto $scheme;
+            proxy_read_timeout  9999;
+        }
+
+        ssl                 on;
+        ssl_certificate     /local/Autopsy/certs/autopsyStackedPEMCerts.pem;
+        ssl_certificate_key /local/Autopsy/certs/autopsy.key;
+    }
+}
+EOF
+cd ../..
--- launch.sh
+++ launch.sh
@@ -5,9 +5,9 @@ mkdir -p uploads
 alias fk="pkill -9 python ; pkill -9 flask"
 alias fl="pkill -9 python ; pkill -9 flask ; flask run --host=0.0.0.0 --no-reload --with-threads"
 alias gk="pkill -9 gunicorn"
-alias gu="pkill -9 gunicorn ; gunicorn -b 127.0.0.1:5000 -t 999999 --threads 64 autopsy:app &"
+alias gu="pkill -9 gunicorn ; gunicorn -b 127.0.0.1:5432 -t 999999 --threads 64 autopsy:app &"
 alias nk="killall nginx"
-alias ng="killall nginx ; /usr/local/nginx/sbin/nginx"
+alias ng="killall nginx ; ../nginx/sbin/nginx"
 source $PWD/venv/bin/activate
 export "FLASK_APP=autopsy.py"
 export "FLASK_DEBUG=0"
